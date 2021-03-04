fe8b741ccccda423b9016ac7227881e114ea0d99
CRenauro <crenauro@optimum.net>
2021-03-03 22:28:31 -0500

    updated js file

--- Develop/app.js
+++ Develop/app.js
@@ -12,8 +12,6 @@ const managerCard = require("./templates/manager.html");
 const internCard = require("./templates/intern.html");
 const engineerCard = require("./templates/engineer.html");
 
-
-
 const uniqueId = 0;
 const fullTeam = [];
 
@@ -92,8 +90,11 @@ inquirer.prompt([{
                         );
                         const managerCardHtml = managerCard(manager);
                         fullTeam.push(managerCardHtml);
+                        console.log(fullTeam)
+                        mainApp();
                     });
                     }
+                    createManager();
                     break;
                 case "Employee":
                     createEmployee = () => {
@@ -144,6 +145,7 @@ inquirer.prompt([{
                         });
                 
                     }
+                    createEmployee();
                     break;
                 case "Engineer":
                     createEngineer = () => {
@@ -202,9 +204,9 @@ inquirer.prompt([{
                             );
                             const engineerCardHtml = engineerCard(engineer);
                             fullTeam.push(engineerCardHtml);
-                            console.log(error);
                         });
                     }
+                    createEngineer();
                     break;
                 case "Intern":
                     createIntern = () => {
@@ -266,6 +268,7 @@ inquirer.prompt([{
                             console.log(error);
                         });
                     }
+                    createIntern();
             }           
 
 })
@@ -291,30 +294,17 @@ function generateHTML() {
 
 };
 
-//call and invoke function to run
+
+
+const managerCardHtml = managerCard(manager);
+fullTeam.push(managerCardHtml);
+console.log(fullTeam);
 mainApp();
 
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// const managerCardHtml = managerCard(manager);
+// fullTeam.push(managerCardHtml);
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// //call and invoke function to run
+// mainApp();
